8e9b42416768b6b282dae1ba371869d7e1069290
meirusfandi <meirusfandi100@gmail.com>
2019-09-15 22:19:03 +0700

    update

--- index.php
+++ index.php
@@ -77,90 +77,87 @@
             $payload['team'] = 'Indonesia';
             $payload['score'] = '1.0';
     
-            if (isset($_POST['title']) == 1 && isset($_POST['message']) == 1 && isset($_POST['regId']) == 1) {
-                // notification title
-                $title = isset($_POST['title']) ? $_POST['title'] : '';
-                
-                // notification message
-                $message = isset($_POST['message']) ? $_POST['message'] : '';
-                
-                // push type - single user / topic
-                $push_type = isset($_POST['push_type']) ? $_POST['push_type'] : '';
-                
-                // whether to include to image or not
-                $include_image = isset($_POST['include_image']) ? TRUE : FALSE;
-        
-                $image = "";
+            if (isset($_POST['submit'])) {
 
-                if ($include_image == TRUE) {
-                    if (isset($_POST['upload']) == 1) {
-                        // upload images first 
+                if (isset($_POST['title']) == 1 && isset($_POST['message']) == 1 && isset($_POST['regId']) == 1) {
+                    // notification title
+                    $title = isset($_POST['title']) ? $_POST['title'] : '';
+                    
+                    // notification message
+                    $message = isset($_POST['message']) ? $_POST['message'] : '';
+                    
+                    // push type - single user / topic
+                    $push_type = isset($_POST['push_type']) ? $_POST['push_type'] : '';
+                    
+                    // whether to include to image or not
+                    $include_image = isset($_POST['include_image']) ? TRUE : FALSE;
+            
+                    $image = '';    
+                    if ($include_image) {
+                        
+                        $filename = $_FILES["gambar"]["name"];
+                        if ($filename != "") {
+                            // upload images first 
+                            $target_dir = "images/";
+                            $target_file = $target_dir.basename($_FILES["gambar"]["name"]);
+                            $uploadOK = 1;
+                            $imagefiletype = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-                        $target_dir = "images/";
-                        $target_file = $target_dir.basename($_FILES["gambar"]["name"]);
-                        $uploadOK = 1;
-                        $imagefiletype = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-
-                        if (isset($_POST["submit"]) == 1) {
-                            $check = getimagesize($_FILES["gambar"]["tmp_name"]);
-                            if ($check !== false) {
-                                echo "Images uploaded ".$check["mime"].".";
-                                $uploadOK = 1;
-                            } else {
-                                echo "Failed to upload images";
+                            // Check if file already exists
+                            // if (file_exists($target_file)) {
+                            //     echo "file allready exists";
+                            //     $uploadOK = 0;
+                            // }
+    
+                            // echo "gambar gak ada di direktori";
+                            // Check file size
+                            if ($_FILES["gambar"]["size"] > 1024000) {
+                                echo "images too large";
                                 $uploadOK = 0;
                             }
-                        }
-
-                        // Check if file already exists
-                        if (file_exists($target_file)) {
-                            echo "Sorry, file already exists.";
-                            $uploadOk = 0;
-                        }
-                        // Check file size
-                        if ($_FILES["fileToUpload"]["size"] > 500000) {
-                            echo "Sorry, your file is too large.";
-                            $uploadOk = 0;
-                        }
-                        // Allow certain file formats
-                        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                        && $imageFileType != "gif" ) {
-                            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-                            $uploadOk = 0;
-                        }
-
-                        // Check if $uploadOk is set to 0 by an error
-                        if ($uploadOk == 0) {
-                            echo "Sorry, your file was not uploaded.";
-                        // if everything is ok, try to upload file
-                        } else {
-                            if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-                                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-                            } else {
-                                echo "Sorry, there was an error uploading your file.";
+    
+                            // echo "ukuran gambar cocok";
+                            // Allow certain file formats
+                            if($imagefiletype != "jpg" && $imagefiletype != "png" && $imagefiletype != "jpeg" && $imagefiletype != "gif" ) {
+                                echo "images type not support";
+                                $uploadOK = 0;
                             }
+                            // echo "format gambar sesuai";
+                            // Check if $uploadOk is set to 0 by an error
+                            if ($uploadOK == 0) {
+                                echo "Sorry, your file was not uploaded.";
+                            // if everything is ok, try to upload file
+                            } else {
+                                if (move_uploaded_file($_FILES["gambar"]["tmp_name"], $target_file)) {
+                                    $image = $_FILES["gambar"]["name"];
+                                } else {
+                                    echo "Sorry, there was an error uploading your file.";
+                                }
+                            }
+                        } else {
+                            $image = "https://api.androidhive.info/images/minion.jpg";
                         }
-                    } else {
-                        $images = "https://api.androidhive.info/images/minion.jpg";
                     }
+    
+                    $push->setTitle($title);
+                    $push->setMessage($message);
+                    $push->setImage($image);
+                    $push->setIsBackground(FALSE);
+                    $push->setPayload($payload);
+            
+                    $json = '';
+                    $response = '';
+                    $json = $push->getPush();
+                    $regId = isset($_POST['regId']) ? $_POST['regId'] : '';
+                    $response = $firebase->send($regId, $json);
+                } else {
+                    $json = '';
+                    $response = '';
                 }
-
-                $push->setTitle($title);
-                $push->setMessage($message);
-                $push->setImage($image);
-                $push->setIsBackground(FALSE);
-                $push->setPayload($payload);
-        
-                $json = '';
-                $response = '';
-                $json = $push->getPush();
-                $regId = isset($_POST['regId']) ? $_POST['regId'] : '';
-                $response = $firebase->send($regId, $json);
             } else {
                 $json = '';
                 $response = '';
             }
-    
         ?>
 
         <div class="container">
